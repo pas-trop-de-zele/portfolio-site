0dc9fe6576a94d76b08541ac52ca1ca1ba2c68ec
pas-trop-de-zele <huy.le09@protonmail.com>
2021-12-07 13:29:03 -0800

    Upload offline files

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        title: "The Little Maruko",
+        subtitle: "Full Stack App",
+        description:
+            "Full stack restaurant website with NodeJS/Express backend. Utilized Twillio API to alert customer on order status",
+        link: "https://github.com/pas-trop-de-zele/little-maruko",
+    },
+    {
+        title: "Pizza Radar",
+        subtitle: "Full Stack App",
+        description:
+            "Full stack app with combined functionalities from Google and Yelp, allowing users to search nearby pizza joints",
+        link: "https://github.com/ktranfullerton2000/cs-362",
+    },
+];
+
+export const skills = [
+    "Python",
+    "C++",
+    "SQL",
+    "DAX",
+    "ReactJs",
+    "NodeJs",
+    "HTML/CSS",
+];
