3d6a87c177c7724c3ffa258c8f4cdb00e4a923dc
Sam <huy.le09@yahoo.com>
2022-03-08 11:06:50 -0800

    Added project

--- src/data.js
+++ src/data.js
@@ -13,18 +13,24 @@ export const jobs = [
 
 export const projects = [
     {
-        title: "The Little Maruko",
+        title: "the-little-maruko",
         subtitle: "Full Stack App",
         description:
             "Full stack restaurant website with NodeJS/Express backend. Utilized Twillio API to alert customer on order status",
         link: "https://github.com/pas-trop-de-zele/little-maruko",
     },
     {
-        title: "Pizza Radar",
+        title: "pizza-radar",
         subtitle: "Full Stack App",
         description:
             "Full stack app with combined functionalities from Google and Yelp, allowing users to search nearby pizza joints",
         link: "https://github.com/ktranfullerton2000/cs-362",
+    },
+    {
+        title: "mysql-passportjs",
+        subtitle: "Utility",
+        description: "Template for using passport js with local strategy",
+        link: "https://github.com/pas-trop-de-zele/mysql-passportjs",
     },
 ];
 
