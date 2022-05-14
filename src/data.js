4cb758a66c36fc404a0490412db00b5aabee2e52
Sam <huy.le09@protonmail.com>
2022-05-14 07:10:18 -0700

    Add mmore project to project section

--- src/data.js
+++ src/data.js
@@ -1,46 +1,60 @@
 export const jobs = [
-    {
-        name: "Palantir",
-        title: "Software Engineer Intern",
-        duration: "Incoming Summer 2022",
-    },
-    {
-        name: "Intel",
-        title: "Software Engineer Intern",
-        duration: "Jan 2021 - August 2021",
-    },
+	{
+		name: "Palantir",
+		title: "Software Engineer Intern",
+		duration: "Incoming Summer 2022",
+	},
+	{
+		name: "Intel",
+		title: "Software Engineer Intern",
+		duration: "Jan 2021 - August 2021",
+	},
 ];
 
 export const projects = [
-    {
-        title: "the-little-maruko",
-        subtitle: "Full Stack App",
-        description:
-            "Full stack restaurant website with NodeJS/Express backend. Utilized Twillio API to alert customer on order status",
-        link: "https://github.com/pas-trop-de-zele/little-maruko",
-    },
-    {
-        title: "pizza-radar",
-        subtitle: "Full Stack App",
-        description:
-            "Full stack app with combined functionalities from Google and Yelp, allowing users to search nearby pizza joints",
-        link: "https://github.com/ktranfullerton2000/cs-362",
-    },
-    {
-        title: "mysql-passportjs",
-        subtitle: "Utility",
-        description: "Template for using passport js with local strategy",
-        link: "https://github.com/pas-trop-de-zele/mysql-passportjs",
-    },
+	{
+		title: "aes128-ecb-cbc",
+		subtitle: "Utilityy",
+		description:
+			"Mini AES128 library that allows ECB/ CBC encryption/ decryption",
+		link: "https://github.com/pas-trop-de-zele/aes128-ecb-cbc",
+	},
+	{
+		title: "virtual-memory-manager",
+		subtitle: "Utility",
+		description:
+			"Simulation of virtual memory system with both FIFO and LRU page replacement policy",
+		link: "https://github.com/pas-trop-de-zele/virtual-memory-manager",
+	},
+	{
+		title: "the-little-maruko",
+		subtitle: "Full Stack App",
+		description:
+			"Full stack restaurant website with NodeJS/Express backend. Utilized Twillio API to alert customer on order status",
+		link: "https://github.com/pas-trop-de-zele/little-maruko",
+	},
+	{
+		title: "pizza-radar",
+		subtitle: "Full Stack App",
+		description:
+			"Full stack app with combined functionalities from Google and Yelp, allowing users to search nearby pizza joints",
+		link: "https://github.com/ktranfullerton2000/cs-362",
+	},
+	{
+		title: "mysql-passportjs",
+		subtitle: "Utility",
+		description: "Template for using passport js with local strategy",
+		link: "https://github.com/pas-trop-de-zele/mysql-passportjs",
+	},
 ];
 
 export const skills = [
-    "Python",
-    "C++",
-    "SQL",
-    "Javascript",
-    "DAX",
-    "ReactJs",
-    "NodeJs",
-    "HTML/CSS",
+	"Python",
+	"C++",
+	"SQL",
+	"Javascript",
+	"DAX",
+	"ReactJs",
+	"NodeJs",
+	"HTML/CSS",
 ];
